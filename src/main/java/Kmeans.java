4a4646cdc1ada9d673fbf072762925b7267958b1
xmkrt <8whZYULe8fvPSFG2buxY>
2017-04-02 16:33:10 +0200

    added kmeans class -> 3 to 5 clusters

---
+++ src/main/java/Kmeans.java
@@ -0,0 +1,199 @@
+import org.knowm.xchart.BubbleChart;
+import org.knowm.xchart.BubbleChartBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by marcel on 02.04.2017.
+ */
+public class Kmeans {
+    private List<Cluster> clusters = new ArrayList<Cluster>();
+    private List<Point> points = new ArrayList<Point>();
+
+    private int n_clusters;
+
+    private Cluster cluster1 = new Cluster(1);
+    private Cluster cluster2 = new Cluster(2);
+    private Cluster cluster3 = new Cluster(3);
+    private Cluster cluster4 = new Cluster(4);
+    private Cluster cluster5 = new Cluster(5);
+
+    Kmeans(List<Point> points, int n_clusters) throws Exception {
+        this.points = points;
+        this.n_clusters = n_clusters;
+        switch (n_clusters) {
+            case 3:
+                clusters.add(cluster1);
+                clusters.add(cluster2);
+                clusters.add(cluster3);
+                break;
+            case 4:
+                clusters.add(cluster1);
+                clusters.add(cluster2);
+                clusters.add(cluster3);
+                clusters.add(cluster4);
+                break;
+            case 5:
+                clusters.add(cluster1);
+                clusters.add(cluster2);
+                clusters.add(cluster3);
+                clusters.add(cluster4);
+                clusters.add(cluster5);
+                break;
+            default:
+                throw new Exception("3-5");
+        }
+
+    }
+
+    void init() {
+        //set random clusters
+        for (Point point : points) {
+            point.setCluster(ThreadLocalRandom.current().nextInt(1, clusters.size() + 1));
+        }
+
+        // add points to clusters
+        for (Point point : points) {
+            switch (point.getCluster()) {
+                case 1:
+                    cluster1.addPoint(point);
+                    break;
+                case 2:
+                    cluster2.addPoint(point);
+                    break;
+                case 3:
+                    cluster3.addPoint(point);
+                    break;
+                case 4:
+                    cluster4.addPoint(point);
+                    break;
+                case 5:
+                    cluster5.addPoint(point);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void calc() {
+        for (Cluster cluster : clusters) {
+            cluster.setRandomCenter();
+        }
+
+        boolean finish = false;
+
+        while (!finish) {
+            //Clear cluster state
+            clearClusters();
+            List lastCenters = getCenters(n_clusters);
+            //Assign points to the closer cluster
+            assignCluster(n_clusters);
+            //Calculate new centroids.
+            calculateCentroids();
+            List currentCentroids = getCenters(n_clusters);
+            //Calculates total distance between new and old Centroids
+            double distance = 0;
+            for (int i = 0; i < lastCenters.size(); i++) {
+                distance += Point.distance((Point) lastCenters.get(i), (Point) currentCentroids.get(i));
+            }
+
+            if (distance < 0.1) {
+                // stop when distance between last and new centers is small
+                finish = true;
+            }
+        }
+
+        for (int i = 0; i < clusters.size(); i++) {
+            System.out.println("Cluster " + (i + 1) + " Center: " + clusters.get(i).getCenter());
+            System.out.println("Points:\n" + clusters.get(i).getPoints());
+        }
+    }
+
+    private void clearClusters() {
+        for (Cluster cluster : clusters) {
+            cluster.clear();
+        }
+    }
+
+    private List getCenters(int k) {
+        List<Point> centers = new ArrayList<Point>(k);
+        for (Cluster cluster : clusters) {
+            Point aux = cluster.getCenter();
+            Point point = new Point(aux.getX(), aux.getY());
+            centers.add(point);
+        }
+        return centers;
+    }
+
+    private void assignCluster(int k) {
+        double max = Double.MAX_VALUE;
+        double min = max;
+        int cluster = 0;
+        double distance;
+
+        for (Point point : points) {
+            min = max;
+            for (int i = 0; i < k; i++) {
+                Cluster c = clusters.get(i);
+                distance = Point.distance(point, c.getCenter());
+                if (distance < min) {
+                    min = distance;
+                    cluster = i;
+                }
+            }
+            point.setCluster(cluster);
+            clusters.get(cluster).addPoint(point);
+        }
+    }
+
+    private void calculateCentroids() {
+        for (Cluster cluster : clusters) {
+            double sumX = 0;
+            double sumY = 0;
+            List<Point> list = cluster.getPoints();
+            int n_points = list.size();
+
+            for (Point point : list) {
+                sumX += point.getX();
+                sumY += point.getY();
+            }
+
+            Point centroid = cluster.getCenter();
+            if (n_points < 0) {
+                double newX = sumX / n_points;
+                double newY = sumY / n_points;
+                centroid.setX(newX);
+                centroid.setY(newY);
+            }
+        }
+    }
+
+
+    BubbleChart draw() {
+        BubbleChart chart = new BubbleChartBuilder().width(800).height(600).title("Viele Punkte mit Cluster").xAxisTitle("X").yAxisTitle("Y").build();
+        int i = 1;
+
+        //converting to xChart Format
+        for (Cluster cluster : clusters) {
+            List<Point> points = cluster.getPoints();
+            List<Double> xData = new ArrayList<Double>();
+            List<Double> yData = new ArrayList<Double>();
+            List<Double> bubbleData = new ArrayList<Double>();
+            for (int j = 0; j < points.size(); j++) {
+                xData.add(points.get(j).getX());
+                yData.add(points.get(j).getY());
+                bubbleData.add(5.0);
+            }
+            chart.addSeries("Cluster " + i, xData, yData, bubbleData);
+            i++;
+        }
+        return chart;
+    }
+}
+
+
+
+
--- src/main/java/chart.java
+++ src/main/java/chart.java
@@ -7,30 +7,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.regex.Pattern;
 
-
 public class chart {
-    private static List<Cluster> clusters = new ArrayList<Cluster>();
-    private static List<Point> points = new ArrayList<Point>();
-
-    private static Cluster cluster1 = new Cluster(1);
-    private static Cluster cluster2 = new Cluster(2);
-    private static Cluster cluster3 = new Cluster(3);
-    private static Cluster cluster4 = new Cluster(4);
-    // private static Cluster cluster5 = new Cluster(5);
-
+    static final int n_clusters = 5;
 
     public static void main(String[] args) {
-        BubbleChart chart = getMyChart();
-        BubbleChart kmeans = getFinishedChart();
-        new SwingWrapper<BubbleChart>(chart).displayChart();
-        new SwingWrapper<BubbleChart>(kmeans).displayChart();
-    }
-
-
-    private static BubbleChart getMyChart() {
+        List<Point> points = new ArrayList<Point>();
         List<String> lines = new ArrayList<String>();
 
         //reading lines with scanner
@@ -43,186 +26,39 @@ public class chart {
             System.err.println(e.getMessage());
         }
 
-        /*try {
-        with Apache IO
-            lines = readLines(new FileReader("data.txt"));
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }*/
-
-        double[] xData = new double[lines.size()];
-        double[] yData = new double[lines.size()];
-        double[] bubbleData = new double[lines.size()];
+        List<Double> xData = new ArrayList<Double>();
+        List<Double> yData = new ArrayList<Double>();
+        List<Integer> bubbleData = new ArrayList<Integer>();
 
         String[] segs;
-        int i = 0;
         for (String s : lines) {
             segs = s.split(Pattern.quote(" "));
-            xData[i] = Double.parseDouble(segs[0]);
-            yData[i] = Double.parseDouble(segs[1]);
-            bubbleData[i] = 5;
-            i++;
+            double x = Double.parseDouble(segs[0]);
+            double y = Double.parseDouble(segs[1]);
+            xData.add(x);
+            yData.add(y);
+            bubbleData.add(5);
+            points.add(new Point(x, y));
         }
-
-        kmeans4(xData, yData);
 
         BubbleChart chart = new BubbleChartBuilder().width(800).height(600).title("Viele Punkte").xAxisTitle("X").yAxisTitle("Y").build();
 
         chart.addSeries(" ", xData, yData, bubbleData);
-        return chart;
-    }
 
-    private static BubbleChart getFinishedChart() {
-        BubbleChart chart = new BubbleChartBuilder().width(800).height(600).title("Viele Punkte mit Cluster").xAxisTitle("X").yAxisTitle("Y").build();
-        int i = 1;
+        Kmeans kmeans = null;
 
-        //converting to xChart Format
-        for (Cluster cluster : clusters) {
-            List<Point> points = cluster.getPoints();
-            List<Double> xData = new ArrayList<Double>();
-            List<Double> yData = new ArrayList<Double>();
-            List<Double> bubbleData = new ArrayList<Double>();
-            for (int j = 0; j < points.size(); j++) {
-                xData.add(points.get(j).getX());
-                yData.add(points.get(j).getY());
-                bubbleData.add(5.0);
-            }
-            chart.addSeries("Cluster " + i, xData, yData, bubbleData);
-            i++;
-        }
-        return chart;
-    }
-
-    private static void kmeans4(double[] xData, double[] yData) {
-        //create  4 clusters and add them to list
-
-        clusters.add(cluster1);
-        clusters.add(cluster2);
-        clusters.add(cluster3);
-        clusters.add(cluster4);
-
-        // set the coordinates for each point from input Data
-        for (int i = 0; i < xData.length; i++) {
-            points.add(new Point(xData[i], yData[i]));
-        }
-        for (Point point : points) {
-            point.setCluster(ThreadLocalRandom.current().nextInt(1, clusters.size() + 1));
+        try {
+            kmeans = new Kmeans(points, n_clusters);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
 
-        addPointsToCluster((ArrayList<Point>) points, cluster1, cluster2, cluster3, cluster4);
+        kmeans.init();
+        kmeans.calc();
 
-        for (Cluster cluster : clusters) {
-            cluster.setRandomCenter();
-        }
 
-        boolean finish = false;
-
-        while (!finish) {
-            //Clear cluster state
-            clearClusters();
-            List lastCenters = getCenters(4);
-            //Assign points to the closer cluster
-            assignCluster(4);
-            //Calculate new centroids.
-            calculateCentroids();
-            List currentCentroids = getCenters(4);
-            //Calculates total distance between new and old Centroids
-            double distance = 0;
-            for (int i = 0; i < lastCenters.size(); i++) {
-                distance += Point.distance((Point) lastCenters.get(i), (Point) currentCentroids.get(i));
-            }
-
-            if (distance < 0.1) {
-                // stop when distance between last and new centers is small
-                finish = true;
-            }
-        }
-
-        for (int i = 0; i < clusters.size(); i++) {
-            System.out.println("Cluster " + (i + 1) + " Center: " + clusters.get(i).getCenter());
-            System.out.println("Points:\n" + clusters.get(i).getPoints());
-        }
-
-    }
-
-    private static void addPointsToCluster(ArrayList<Point> points, Cluster cluster1, Cluster cluster2, Cluster cluster3, Cluster cluster4) {
-        // add points to clusters
-        for (Point point : points) {
-            switch (point.getCluster()) {
-                case 1:
-                    cluster1.addPoint(point);
-                    break;
-                case 2:
-                    cluster2.addPoint(point);
-                    break;
-                case 3:
-                    cluster3.addPoint(point);
-                    break;
-                case 4:
-                    cluster4.addPoint(point);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    private static void clearClusters() {
-        for (Cluster cluster : clusters) {
-            cluster.clear();
-        }
-    }
-
-    private static List getCenters(int k) {
-        List<Point> centers = new ArrayList<Point>(k);
-        for (Cluster cluster : clusters) {
-            Point aux = cluster.getCenter();
-            Point point = new Point(aux.getX(), aux.getY());
-            centers.add(point);
-        }
-        return centers;
-    }
-
-    private static void assignCluster(int k) {
-        double max = Double.MAX_VALUE;
-        double min = max;
-        int cluster = 0;
-        double distance;
-
-        for (Point point : points) {
-            min = max;
-            for (int i = 0; i < k; i++) {
-                Cluster c = clusters.get(i);
-                distance = Point.distance(point, c.getCenter());
-                if (distance < min) {
-                    min = distance;
-                    cluster = i;
-                }
-            }
-            point.setCluster(cluster);
-            clusters.get(cluster).addPoint(point);
-        }
-    }
-
-    private static void calculateCentroids() {
-        for (Cluster cluster : clusters) {
-            double sumX = 0;
-            double sumY = 0;
-            List<Point> list = cluster.getPoints();
-            int n_points = list.size();
-
-            for (Point point : list) {
-                sumX += point.getX();
-                sumY += point.getY();
-            }
-
-            Point centroid = cluster.getCenter();
-            if (n_points < 0) {
-                double newX = sumX / n_points;
-                double newY = sumY / n_points;
-                centroid.setX(newX);
-                centroid.setY(newY);
-            }
-        }
+        BubbleChart finishedChart = kmeans.draw();
+        new SwingWrapper<BubbleChart>(chart).displayChart();
+        new SwingWrapper<BubbleChart>(finishedChart).displayChart();
     }
 }
